3ba804e509c8b0e8ccf58554c5a4c5f60b2ec7f5
kaiser.ren@gmail.com <kaiser.ren@gmail.com@4d133387-9cbb-5f73-990a-3ac0ff6c78c2>
2012-12-15 01:29:12 +0000

    tag for back_end v0.04 decode successfully, PER is 0.5% when one byte sending per 500ms; PER is 5% when 6-byte frame sending per 500ms.

---
+++ tags/back_end_V0.04/Applications/TAL_Examples/Performance_Test/Src/decode.c
@@ -0,0 +1,435 @@
+/* ----------------------------------------------------------------------------
+ *         ENCODE SOURCE CODE
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012, kren
+ */
+
+/**
+ * \file decode.c
+ *
+ * Implements machester modulation.
+ *
+ */
+/*----------------------------------------------------------------------------
+ *        Headers
+ *----------------------------------------------------------------------------*/
+#include "hal.h"
+
+
+/*----------------------------------------------------------------------------
+ *        Variable
+ *----------------------------------------------------------------------------*/
+decode_t dec;
+uint16_t cur_stamp = 0;
+uint8_t cur_ovfw = 0;
+uint8_t	ovfw = 0;
+uint8_t	acc_occur = 0;
+
+static void dec_update_tmr(void);
+
+/*----------------------------------------------------------------------------
+ *        ISR Handler
+ *----------------------------------------------------------------------------*/
+/**
+ * @brief ACC_ISR, load TMR2 cnt and ACSR for further operation, decode_state()
+ *
+ */
+#if (PAL_TYPE==ATTINY88)
+ISR(ANA_COMP_vect)
+{
+  	uint8_t sreg;
+
+	sreg = SREG; 	// Save global interrupt flag
+	__disable_interrupt();	// Disable interrupts
+
+	pal_led(LED_1, LED_TOGGLE);
+
+  	acc_occur = 1;		// set the flag for further operation in while(1)
+  	cur_stamp = TCNT0;	// load TMR2 count
+	dec.acsr  = ACSR;   // load ACSR status
+
+	SREG = sreg;	// Restore global interrupt flag
+}
+
+/**
+ * @brief TMR0 Overflow interrupt.
+ *
+ */
+ISR(TIMER0_OVF_vect)
+{
+	cur_ovfw++;
+}
+
+#else //ATMEGA1281
+
+#if HAL_USE_ACC_CAP>0
+ISR(ANALOG_COMP_vect)
+{
+  	uint8_t sreg;
+
+	pal_led(LED_1, LED_TOGGLE);
+
+	sreg = SREG; 	// Save global interrupt flag
+	cli();	// Disable interrupts
+
+  	acc_occur = 1;		// set the flag for further operation in while(1)
+  	cur_stamp = TCNT1;	// load TMR2 count
+	cur_ovfw = ovfw;
+	dec.acsr  = ACSR;   // load ACSR status
+
+	SREG = sreg;	// Restore global interrupt flag
+}
+#endif//HAL_USE_ACC_CAP
+
+#if DECODE_USED_TMR_ID==2
+/**
+ * @brief TMR2 Overflow interrupt.
+ *
+ */
+ISR(TIMER2_OVF_vect)
+{
+	ovfw++;
+}
+
+#elif DECODE_USED_TMR_ID==1
+/**
+ * @brief TMR1 Overflow interrupt.
+ *
+ */
+ISR(TIMER1_OVF_vect)
+{
+	ovfw++;
+}
+#else
+    #error "Unsupported Decode Timer Id"
+#endif//DECODE_USED_TMR_ID
+#endif//ATTINY88
+
+
+
+/*----------------------------------------------------------------------------
+ *        Exported functions
+ *----------------------------------------------------------------------------*/
+/**
+ * @brief Initialization for AC compare vector
+ *
+ */
+#if (PAL_TYPE==ATTINY88)
+void ac_init(void)
+{
+   /* Select ADC3 as negtive input, mcu pin PF3, module pin 30. */
+	ADCSRB = _BV(ACME);
+	ADCSRA &= ~_BV(ADEN);
+	ADMUX = _BV(MUX0) | _BV(MUX1);
+
+	/* initial global variable. */
+	memset(&ac_cap_para, 0, sizeof(ac_cap_t));
+
+#if HAL_USE_ACC_CAP==0
+	/* Config AC's setting, ACO enable and make it connect with Tmr1's
+	capture input, toggle trigger. */
+	ACSR = _BV(ACIC);
+	//noise canceller, falling edge detection, prescaler is clkio/8
+	TCCR1B = _BV(ICNC1) | _BV(ICES1) | _BV(CS11);
+	//clear potential capture int flag
+	TIFR1 = _BV(ICF1);
+	//enable capture int
+	TIMSK1 = _BV(ICIE1);
+#else
+	memset(&dec, 0, sizeof(decode_t) );
+	/**
+	 *	Acc_Int enable,
+	 *	ACC_Int flag clear
+	 *  Acc_Int mode is edge sensitive.
+	**/
+	ACSR = _BV(ACIE) | _BV(ACI);
+
+	/**
+	 *	PRESCALSE: 128
+	 *	F_CPU: 4MHz
+	 * Tick:  4MHz/128 = 32us.
+	 *	enable TMR0 overflow interrupt, 256*32us = 8192us
+	**/
+	TCCR0A = _BV(CS01) | _BV(CS00) ;
+	TIMSK0 = _BV(TOIE0) ;
+
+#endif
+}
+
+#else //ATMEGA1281
+void ac_init(void)
+{
+   /* Select ADC3 as negtive input, mcu pin PF3, module pin 30. */
+	ADCSRB = _BV(ACME);
+	ADCSRA &= ~_BV(ADEN);
+	ADMUX = _BV(MUX0) | _BV(MUX1);
+
+	/* initial global variable. */
+	memset(&ac_cap_para, 0, sizeof(ac_cap_t));
+
+#if HAL_USE_ACC_CAP==0
+	/* Config AC's setting, ACO enable and make it connect with Tmr1's
+	capture input, toggle trigger. */
+	ACSR = _BV(ACIC);
+	//noise canceller, falling edge detection, prescaler is clkio/8
+	TCCR1B = _BV(ICNC1) | _BV(ICES1) | _BV(CS11);
+	//clear potential capture int flag
+	TIFR1 = _BV(ICF1);
+	//enable capture int
+	TIMSK1 = _BV(ICIE1);
+#else
+	memset(&dec, 0, sizeof(decode_t) );
+	/**
+	 *	Acc_Int enable,
+	 *	ACC_Int flag clear
+	 *  Acc_Int mode is edge sensitive.
+	**/
+	ACSR = _BV(ACIE) | _BV(ACI);
+
+#if DECODE_USED_TMR_ID==2
+	TCCR2B = DECODE_TMR_CLK_SRC_PRESCALER_REG ;
+	//TIMSK2 = _BV(TOIE2) ;
+#elif DECODE_USED_TMR_ID==1
+    TCCR1B = DECODE_TMR_CLK_SRC_PRESCALER_REG ;
+    //TIMSK1 = _BV(TOIE1) ;
+#else
+    #error "Unsupported Decode Timer Id"
+#endif//DECODE_USED_TMR_ID
+	
+#endif//HAL_USE_ACC_CAP
+}
+#endif//ATTINY88
+
+/**
+ * @brief Calculate the interval between ACC_ISR.
+ *
+ */
+uint16_t cal_interval(void)
+{
+  	uint16_t inv;	//interval
+
+	// equal condition, no overflow occur
+	if(cur_ovfw == dec.prev_ovfw)
+	  	return (cur_stamp - dec.prev_stamp);
+
+	inv = cur_ovfw - dec.prev_ovfw;	//get the overflow
+	if(inv > 1){
+	  	dec_update_tmr();
+		return 256;		//this is invalid interval, too huge.
+	}
+	//only one overflow occur
+	inv = 65535 - dec.prev_stamp;
+	inv = cur_stamp + inv;
+	return (inv);
+}
+
+/**
+ * @brief update dec's timer stamp.
+ *
+ */
+static void dec_update_tmr(void)
+{
+	dec.prev_stamp = cur_stamp;		//reload time stamp
+	dec.prev_ovfw = cur_ovfw;        //reload overflow cnt
+}
+
+
+/**
+ * @brief decode state machine.
+ *
+ */
+void decode_machine(void)
+{
+  	uint16_t inv;	//interval
+
+	inv = cal_interval();
+	//printf("%u\r\n", inv);
+  	switch (dec.state){
+		case Waiting:
+		  	if( dec.acsr & ( 1 << ACO) )	{	//rising
+				dec.state = Sta0;
+			}
+			dec_update_tmr();
+			break;
+			//
+		case Sta0:
+		  	if ( !( dec.acsr & ( 1 << ACO) )	)	{//falling
+		  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX))
+				  	dec.state = Sta1;
+				else
+				  	dec.state = Waiting;
+			}
+			else{
+				dec.state = Waiting;
+			}
+			dec_update_tmr();
+			break;
+			//
+		case Sta1:
+		  	if ( dec.acsr & ( 1 << ACO) )	{	//rising
+		  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX))
+				  	dec.state = Sta2;
+				else	//falling
+				  	dec.state = Waiting;
+			}
+			else{
+				dec.state = Waiting;
+			}
+			dec_update_tmr();
+			break;
+			//
+		case Sta2:
+		  	if ( !( dec.acsr & ( 1 << ACO) ) )	{	//falling
+		  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX))
+				  	dec.state = Sta3;
+				else
+				  	dec.state = Waiting;
+			}
+			else{
+				dec.state = Waiting;
+			}
+			dec_update_tmr();
+			break;
+			//
+		case Sta3:
+		  	if ( dec.acsr & ( 1 << ACO) )	{ //rising
+		  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX))
+				  	dec.state = Bit7;
+				else									//falling
+				  	dec.state = Waiting;
+			}
+			else{
+				dec.state = Waiting;
+			}
+			dec_update_tmr();
+			break;
+			//
+		case Bit7:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT7 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT7 ) ;
+				dec.state = Bit6;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit6:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT6 ) ;
+				else						 	//falling
+				  	dec.data &= ~( 1 << BIT6 ) ;
+				dec.state = Bit5;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit5:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT5 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT5 ) ;
+				dec.state = Bit4;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit4:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT4 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT4 ) ;
+				dec.state = Bit3;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit3:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT3 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT3 ) ;
+				dec.state = Bit2;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit2:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT2 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT2 ) ;
+				dec.state = Bit1;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit1:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT1 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT1 ) ;
+				dec.state = Bit0;
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		case Bit0:
+	  		if( ( inv >= DECODE_TMR_FREQ_2KHZ_MIN ) && (inv <= DECODE_TMR_FREQ_2KHZ_MAX)){
+		  		dec_update_tmr();
+				if ( dec.acsr & ( 1 << ACO) ) 	//rising
+				  	dec.data |= ( 1 << BIT0 ) ;
+				else							//falling
+				  	dec.data &= ~( 1 << BIT0 ) ;
+				sio_putchar(dec.data);
+				dec.state = Waiting;
+
+			}
+			else if (inv > DECODE_TMR_FREQ_2KHZ_MAX ) {
+				dec.state = Waiting;
+				dec_update_tmr();
+			}
+			break;
+			//
+		default:
+	  		break;
+			//
+
+	}
+}
+//end of file
---
+++ tags/back_end_V0.04/Applications/TAL_Examples/Performance_Test/Src/main.c
@@ -0,0 +1,162 @@
+/**
+ * @file main.c
+ *
+ * @brief  Main of TAL Example - Performance_Test
+ *
+ * $Id: main.c 22695 2010-07-27 13:13:57Z sschneid $
+ *
+ * @author    Atmel Corporation: http://www.atmel.com
+ * @author    Support email: avr@atmel.com
+ */
+/*
+ * Copyright (c) 2009, Atmel Corporation All rights reserved.
+ *
+ * Licensed under Atmel's Limited License Agreement --> EULA.txt
+ */
+
+/* === INCLUDES ============================================================ */
+#include "hal.h"
+
+/* === TYPES =============================================================== */
+
+
+/* === MACROS ============================================================== */
+#define VER_MAJOR	0
+#define VER_MINOR 4
+
+
+
+/* === PROTOTYPES ========================================================== */
+static void print_main_menu(void);
+static void get_analog_cmp_freq(void);
+void data_moduler(uint8_t data);
+
+/* === GLOBALS ============================================================= */
+#if (PAL_TYPE==ATMEGA1281)
+__root __farflash  	uint32_t		Bf_Appflag 	@0x1DFFC = 0xa5a55a5al;
+#endif
+
+/* === IMPLEMENTATION ====================================================== */
+/**
+ * @brief Main function of the Performance_Test application
+ */
+int main(void)
+{
+	/* initial hal layer. */
+	hal_init();
+	
+	printf("\r\n-- Welcome to ATmega Test V%d.%02d --\r\n", VER_MAJOR, VER_MINOR);
+	printf("-- Build on: "__TIME__" @"__DATE__"\r\n");
+	printf("-- F_CPU: %lu\r\n", F_CPU);
+
+	/* Endless while loop */
+	while (1)
+   {
+		//print_main_menu();
+#if DECODE_USED_TMR_ID==2
+	 	if( TIFR2 & _BV(TOV2)){
+	 		TIFR2 = 0xFF;
+			ovfw++;
+		}
+#elif DECODE_USED_TMR_ID==1
+		if( TIFR1 & _BV(TOV1)){
+	 		TIFR1 = 0xFF;
+			ovfw++;
+		}
+#else
+	#error "Unsupported Decode Timer Id"		
+#endif//DECODE_USED_TMR_ID
+	  	// there is acc occur, goto state machine to parser.
+	  	if( acc_occur ){
+		  	pal_led(LED_0, LED_ON);
+	  		acc_occur = 0;
+			decode_machine();
+			pal_led(LED_0, LED_OFF);
+		}
+   }
+}
+
+/*
+ * @brief print main menu.
+*/
+static void print_main_menu(void)
+{
+  	uint8_t input;
+	//uint8_t sreg;
+	
+	/* Get input from terminal program / user. */
+ 	input = sio_getchar();
+   input = toupper(input);
+
+   /* Handle input from terminal program. */
+   switch (input)
+   {	
+  		case 'F':
+       	get_analog_cmp_freq();
+         break;
+        //
+		case 'C':
+	  		pwm_uninit();
+		  	break;
+			//
+		case 'E':
+		  	pwm_set_freq(PWM_TMR0_CMP_OUT_FREQ);
+			break;
+			//
+		case '1':
+		  	pwm_set_freq(1000);
+			break;
+			//	
+		case '2':
+		  	pwm_set_freq(2000);
+			break;
+			//
+		case 'M':
+	  		data_moduler(0x5a);
+			break;
+			//
+		default:
+	  		break;
+			//
+	}
+	/* Restore global interrupt flag */
+	//SREG = sreg;
+}
+
+void data_moduler(uint8_t data)
+{
+  	int8_t i;
+	
+	pwm_para.data = data;
+	for(i = 7; i >= 0; i--){
+	  	pwm_set_freq( ( (pwm_para.data >> i) & 0x01) * 1000 + 1000 );
+	  	while(pwm_para.toggle_cnt);
+	}
+}
+
+/**
+ * @brief Sub-menu to get frequency.
+ */
+static void get_analog_cmp_freq(void)
+{
+  	uint16_t freq;
+	
+	freq = 1000000ul/ac_cap_para.interval/2;
+	printf("\r\nFreq:%u\r\n", freq);
+}
+
+
+/**
+ * @brief Sub-menu to print sensor data
+ */
+static void get_sensor_data(void)
+{
+    //uint16_t bat_mon;
+
+    //bat_mon = tfa_get_batmon_voltage();
+    //printf("\r\nBattery monitor: U = %" PRIu16 ".%" PRIu16 " V\r\n", bat_mon/1000, bat_mon%1000);
+}
+
+
+
+/* EOF */
