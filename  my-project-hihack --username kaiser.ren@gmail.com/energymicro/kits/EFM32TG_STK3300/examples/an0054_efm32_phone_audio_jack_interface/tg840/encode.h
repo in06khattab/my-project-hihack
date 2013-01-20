a28381a00f33f45006cd5ab2b543a0a4a4071910
kaiser.ren <kaiser.ren@gmail.com>
2013-01-20 13:04:41 +0000

    my-project-hijack energy_micro 1. NEW: add encode/decode files. 2. NEW: add Doc/HiJack_Phone_UI_Requirement_Definition.docx

---
+++  my-project-hihack --username kaiser.ren@gmail.com/energymicro/kits/EFM32TG_STK3300/examples/an0054_efm32_phone_audio_jack_interface/tg840/encode.h
@@ -0,0 +1,90 @@
+/* ----------------------------------------------------------------------------
+ *         ENCODE SOURCE CODE
+ * ----------------------------------------------------------------------------
+ * Copyright (c) 2012, kren
+ */
+
+/**
+ * \file encode.h
+ *
+ * Implements machester modulation.
+ *
+ */
+
+#ifndef ENCODE_H
+#define ENCODE_H
+/*----------------------------------------------------------------------------
+ *        Header
+ *----------------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <assert.h>
+#include "main.h"
+#include "board.h"
+
+/*----------------------------------------------------------------------------
+ *        Typedef
+ *----------------------------------------------------------------------------*/
+typedef enum next_bit_tag
+{
+	CLR = 0,
+	SET = 1
+}next_bit_t;
+
+typedef enum factor_tag
+{
+	Div1 = 1,
+	Div2 = 2  	
+}factor_t;
+
+typedef struct module_tag
+{
+  	uint8_t 	data;
+	state_t state;
+	next_bit_t  cur;
+    factor_t	factor;
+	uint8_t		reverse;
+}encode_t;
+
+typedef struct _usart_rx_tag
+{
+  	uint8_t count;
+	uint8_t head;
+	uint8_t tail;
+	uint8_t buff[20];
+}us_rx_t;
+
+/*----------------------------------------------------------------------------
+ *        Macros
+ *----------------------------------------------------------------------------*/
+/** Reference voltage for DACC,in mv*/
+#define VOLT_REF   (3300)
+
+/** The maximal digital value*/
+#define MAX_DIGITAL (4095)
+
+/** SAMPLES per cycle*/
+#define SAMPLES (100)
+
+/*----------------------------------------------------------------------------
+ *        External Variable
+ *----------------------------------------------------------------------------*/
+//extern encode_t enc;
+//extern uint8_t index_sample;
+//extern uint8_t ticker;
+extern us_rx_t us1;
+extern const int16_t sine_data[SAMPLES];
+extern uint16_t amplitude ;
+extern uint16_t frequency ;
+
+/*----------------------------------------------------------------------------
+ *        External Function
+ *----------------------------------------------------------------------------*/
+void encode_machine(void);
+uint8_t us1_get_count(void);
+uint8_t us1_get_char(void);
+void _ConfigureCom( void );
+void enc_init(void);
+#endif /* ENCODE_H */
+//end of file
