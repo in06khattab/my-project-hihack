6ff40df632881ecb3d1f2f0baa48665a5d95fe43
kaiser.ren@gmail.com <kaiser.ren@gmail.com@4d133387-9cbb-5f73-990a-3ac0ff6c78c2>
2013-01-25 07:58:25 +0000

    my-project-hijack EFM32TG V0.02 1. CHG: 210 USART0 initial. 2. CHG: 210 encode part.

--- trunk/ my-project-hihack --username kaiser.ren@gmail.com/energymicro/kits/EFM32TG_STK3300/examples/Senfone/tg210/encode.c
+++ trunk/ my-project-hihack --username kaiser.ren@gmail.com/energymicro/kits/EFM32TG_STK3300/examples/Senfone/tg210/encode.c
@@ -14,399 +14,324 @@
  *        Headers
  *----------------------------------------------------------------------------*/
 #include "encode.h"
-#include "xplained.h"
+#include "com.h"
+
 
 /*----------------------------------------------------------------------------
- *        Variable
+ *        local variable
  *----------------------------------------------------------------------------*/
 /** encode structure */
 static encode_t enc;
-/** store odd parity consequence */
-static uint8_t odd;	
-/** current index_sample */
-static uint8_t index_sample = 0;
-/** ticker counter. */
-static uint8_t ticker = 0;
-
-/** channel 0 */
-uint8_t DACC_channel_sine = DACC_CHANNEL_0;
-/** frequency */
-uint16_t frequency = 0;
-/** amplitude */
-uint16_t amplitude = 0;
+static uint8_t enc_odd ;
+static uint8_t dbg_led = 0 ;
 
 /*----------------------------------------------------------------------------
- *        Static functions
+ *        global variable
+ *----------------------------------------------------------------------------*/
+uint32_t ticker = 0 ;
+
+/*----------------------------------------------------------------------------
+ *        local functions
+ *----------------------------------------------------------------------------*/
+static void HIJACK_CompareConfig(HIJACK_OutputMode_t outputMode);
+
+/*----------------------------------------------------------------------------
+ *        global functions
  *----------------------------------------------------------------------------*/
 void enc_parser(uint8_t bit_msk);
-static void _ConfigureTc0( uint32_t freq );
+void encode_machine(void);
 /*----------------------------------------------------------------------------
  *        ISR Handler
  *----------------------------------------------------------------------------*/
-/**
- *  \brief Interrupt handler for DACC.
- *
- * Server routine when DACC complete the convertion.
- */
-void DAC_IrqHandler(void)
+/***************************************************************************//**
+ * @brief
+ *   Timer1 IRQHandler.
+ ******************************************************************************/
+void TIMER1_IRQHandler(void)
 {
-	uint32_t status ;
+  //static volatile uint8_t currentPin = 1;
+  //static volatile uint8_t currentSym = 1;
+  static volatile uint8_t txParity = 0;
+  uint32_t irqFlags;
 
-    status = DACC_GetStatus( DACC ) ;
+  /* Clear all pending IRQ flags. */
+  irqFlags = TIMER_IntGet(HIJACK_TX_TIMER);
+  TIMER_IntClear(HIJACK_TX_TIMER, irqFlags);
 
-    /* if conversion is done*/
-    if ( (status & DACC_IER_EOC) == DACC_IER_EOC ){
-		if ( index_sample >= SAMPLES ){
-		  	if(0 == enc.reverse)
-				encode_machine();
-			index_sample = 0;
-			ticker = 0;
-		}
-		else if( ( (SAMPLES/2) == index_sample ) && (Div2 == enc.factor ) ){
-			encode_machine();
-			ticker = 0;
-		}
-		else if( ( (SAMPLES/2) == index_sample ) && ( 1 == enc.reverse ) ){
-		  	enc.reverse = 0;
-			encode_machine();
-			ticker = 0;
-		}
-		DACC->DACC_IDR = DACC_IER_EOC;
-	}
+  ticker++;
 
+  /* Reset the counter and the compare value. */
+  TIMER_CounterSet(HIJACK_TX_TIMER, 0);
+  TIMER_CompareSet(HIJACK_TX_TIMER, 0, 500ul*HIJACK_NUM_TICKS_PER_1US*HIJACK_ENC_CARRIER_FREQ_1KHZ/HIJACK_ENC_CARRIER_FREQ_CONF);
+  //BSP_LedToggle(0);
+
+  encode_machine();
 }
 
-/**
- *  \brief Interrupt handler for UART0.
- *
- */
-void UART0_IrqHandler(void)
+/***************************************************************************//**
+ * @brief
+ *   Configure the compare channel for the HiJack.
+ ******************************************************************************/
+static void HIJACK_CompareConfig(HIJACK_OutputMode_t outputMode)
 {
-    uint32_t status;
+  TIMER_InitCC_TypeDef txTimerCapComChConf =
+  { timerEventEveryEdge,      /* Event on every capture. */
+    timerEdgeRising,          /* Input capture edge on rising edge. */
+    timerPRSSELCh0,           /* Not used by default, select PRS channel 0. */
+    timerOutputActionNone,    /* No action on underflow. */
+    timerOutputActionNone,    /* No action on overflow. */
+    timerOutputActionSet,     /* No action on match. */
+    timerCCModeCompare,       /* Configure capture channel. */
+    false,                    /* Disable filter. */
+    false,                    /* Select TIMERnCCx input. */
+    true,                     /* Output high when counter disabled. */
+    false                     /* Do not invert output. */
+  };
 
-    /* Read USART status*/
-    status = UART0->UART_SR;
 
-    /* Receive byte is stored in buffer. */
-    if ((status & UART_SR_RXRDY) == UART_SR_RXRDY) {
-		if(us1.count < 20){
-	    	us1.buff[us1.head++] = UART_GetChar();
-			us1.count++;
-			if(us1.head >= 20)
-				us1.head = 0;
-	  	}
-		else{
-			us1.buff[us1.head] = UART_GetChar();
-		}
-    }
+  if (hijackOutputModeSet == outputMode)
+  {
+    txTimerCapComChConf.cmoa = timerOutputActionSet;
+  }
+  else if (hijackOutputModeClear == outputMode)
+  {
+    txTimerCapComChConf.cmoa = timerOutputActionClear;
+  }
+  else if (hijackOutputModeToggle == outputMode)
+  {
+    txTimerCapComChConf.cmoa = timerOutputActionToggle;
+  }
+  else
+  {
+    /* Config error. */
+    txTimerCapComChConf.cmoa = timerOutputActionNone;
+  }
+
+  TIMER_InitCC(HIJACK_TX_TIMER, 0, &txTimerCapComChConf);
 }
 
-/**
- *  \brief Interrupt handler for SysTick.
- *
- */
-void SysTick_Handler( void )
-{
-	uint16_t value;
-
-    {
-		if ( 0 == ( ticker%enc.factor) ){
-			value = sine_data[index_sample++] * amplitude / (MAX_DIGITAL/2) + MAX_DIGITAL/2;
-        	DACC_SetConversionData(DACC, value ) ;
-			DACC->DACC_IER = DACC_IER_EOC;
-		}
-		ticker++;
-    }
-}
 
 /**
- *  \brief Interrupt handler for TC0.
- *
- */
-void TC0_IrqHandler( void )
-{
-  	uint32_t status ;
-    status = REG_TC0_SR0 ;
-
-	if ( (status & TC_SR_CPCS) == TC_SR_CPCS ){
-		XplnLED_Toggle(1);	//LED1 toggle
-	}
-
-}
-
-/**
- * \brief DAC initialization.
+ * \brief encode part initial.
  */
 void enc_init(void)
 {
-	/* initialize amplitude and frequency */
-    amplitude = MAX_DIGITAL / 2;
-    frequency = HIJACK_CARRIER_FREQ_CONF;
+  static const TIMER_Init_TypeDef txTimerInit =
+  { false,                  /* Don't enable timer when init complete. */
+    false,                  /* Stop counter during debug halt. */
+    HIJACK_TIMER_RESOLUTION,/* ... */
+    timerClkSelHFPerClk,    /* Select HFPER clock. */
+    false,                  /* Not 2x count mode. */
+    false,                  /* No ATI. */
+    timerInputActionNone,   /* No action on falling input edge. */
+    timerInputActionNone,   /* No action on rising input edge. */
+    timerModeUp,            /* Up-counting. */
+    false,                  /* Do not clear DMA requests when DMA channel is active. */
+    false,                  /* Select X2 quadrature decode mode (if used). */
+    false,                  /* Disable one shot. */
+    false                   /* Not started/stopped/reloaded by other timers. */
+  };
 
-    /*10 us timer*/
-    SysTick_Config( BOARD_MCK / (frequency * SAMPLES) ) ;
-	//_ConfigureTc0( HIJACK_CARRIER_FREQ_8KHZ );
-	//TC_Start( TC0, 0 ) ;
-	
-    /* Initialize DACC */
-    DACC_Initialize( DACC,
-                    ID_DACC,
-                    0, /* Hardware triggers are disabled */
-                    0, /* External trigger */
-                    0, /* Half-Word Transfer */
-                    0, /* Normal Mode (not sleep mode) */
-                    BOARD_MCK,
-                    8, /* refresh period */
-                    0, /* Channel 0 selection */
-                    0, /* Tag Selection Mode disabled */
-                    16 /*  value of the start up time */);
+  /* Ensure core frequency has been updated */
+  SystemCoreClockUpdate();
 
-	/* enable NVIC_DACC. */
-	NVIC_EnableIRQ( DACC_IRQn ) ;
-	NVIC_SetPriority(DACC_IRQn, 5);
-	
-	/* variable initial. */
-	enc.data = 0;
-	enc.state = Waiting;
-	enc.cur = SET;
-	enc.factor = Div1;
-	enc.reverse = 0;
-	ticker = 0;
-	index_sample = 0;
-	
-	/*Enable  channel for potentiometer*/
-    DACC_EnableChannel( DACC, DACC_channel_sine ) ;
+  /* Enable peripheral clocks. */
+  CMU_ClockEnable(cmuClock_HFPER, true);
+  CMU_ClockEnable(HIJACK_TX_TIMERCLK, true);
 
-    /*initialize the DACC_CDR*/
-    DACC_SetConversionData( DACC,sine_data[90]*amplitude/(MAX_DIGITAL/2)+MAX_DIGITAL/2);
-	 DACC->DACC_IER = DACC_IER_EOC;	//Enable DACC end-of-convertion interrupt
+  /* Configure Rx timer. */
+  TIMER_Init(HIJACK_TX_TIMER, &txTimerInit);
+
+  /* Configure Tx timer output compare channel 0. */
+  HIJACK_CompareConfig(hijackOutputModeToggle);
+  TIMER_CompareSet(HIJACK_TX_TIMER, 0, 500ul*HIJACK_NUM_TICKS_PER_1US*HIJACK_ENC_CARRIER_FREQ_1KHZ/HIJACK_ENC_CARRIER_FREQ_CONF);
+
+  /* Route the capture channels to the correct pins, enable CC0. */
+  HIJACK_TX_TIMER->ROUTE = TIMER_ROUTE_LOCATION_LOC4 | TIMER_ROUTE_CC0PEN;
+
+  /* Tx: Configure the corresponding GPIO pin (PortD, Ch6) as an input. */
+  GPIO_PinModeSet(HIJACK_TX_GPIO_PORT, HIJACK_TX_GPIO_PIN, gpioModePushPull, 0);
+
+  /* Enable Tx timer CC0 interrupt. */
+  NVIC_EnableIRQ(TIMER1_IRQn);
+  TIMER_IntEnable(HIJACK_TX_TIMER, TIMER_IF_CC0);
+
+  /* Enable the timer. */
+  TIMER_Enable(HIJACK_TX_TIMER, true);
 }
 
-/*----------------------------------------------------------------------------
- *        Exported functions
- *----------------------------------------------------------------------------*/
-/**
- *  \brief TC0 configuration
- *
- * Configures Timer Counter 0 (TC0) to generate an interrupt every second. This
- * interrupt will be used to display the number of bytes received on the USART.
- */
-
-static void _ConfigureTc0( uint32_t freq )
-{
-    /* Enable TC0 peripheral clock*/
-    PMC_EnablePeripheral( ID_TC0 ) ;
-
-    /*
-	 	CLK SRC: MCK/2, 64MHz/2 = 32MHz.
-	 	RC Compare trigger, RC Compare resets the counter and starts the counter clock.
-	 */
-    TC_Configure( TC0, 0, TC_CMR_TCCLKS_TIMER_CLOCK1 | TC_CMR_CPCTRG ) ;
-
-    TC0->TC_CHANNEL[0].TC_RC = 32*100 ;
-
-    /* Configure interrupt on RC compare*/
-    TC0->TC_CHANNEL[0].TC_IER = TC_SR_CPCS ;
-
-	NVIC_DisableIRQ( TC0_IRQn ) ;
-    NVIC_ClearPendingIRQ( TC0_IRQn ) ;
-    NVIC_SetPriority( TC0_IRQn, 0 ) ;
-    NVIC_EnableIRQ( TC0_IRQn ) ;
-
-}
-
-/**
- *  \brief UART0 hardware configuration
- *
- * Configures UART0 in hardware handshaking mode, asynchronous, 8 bits, 1 stop
- * bit, no parity, 115200 bauds and enables its transmitter and receiver.
- */
-void _ConfigureCom( void )
-{
-  	/* Enaboe UART0 interrupt*/
-    NVIC_EnableIRQ( UART0_IRQn ) ;
-	/* Enable UART0 RXREADY interrupt. */
-	UART0->UART_IER = UART_IER_RXRDY ;
-}
-/*
- * Get us1 buffer amount.
-*/
-uint8_t us1_get_count(void)
-{
-  	return (us1.count);
-}
-
-/*
- * Get a charactors.
-*/
-uint8_t us1_get_char(void)
-{
-  	uint8_t temp;
-	
-	if(us1.count){
-    	temp = us1.buff[us1.tail++];
-		us1.count--;
-		if(us1.tail >= 20){
-			us1.tail = 0;
-		}
-  	}
-    return temp;
-}
 /*
  * Find proper factor depending on state and bit mask.
 */
 
-void enc_parser(uint8_t bit_msk)
+void findParam(uint8_t bit_msk, state_t state)
 {
-  	if( ( enc.data & ( 1 << bit_msk) ) && (SET == enc.cur) ){
-	  	odd++;	//bit is one, odd cnt increacement
-  		enc.factor = Div1;
-		if( 50 == index_sample )
-		  	enc.reverse = 1;
+	if( 0 == ticker % 2){
+		if( enc.data & ( 1 << bit_msk) ){
+			enc.edge = rising;
+         HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, for rising
+			enc_odd++;
+			dbg_led = 1;
+		}
+		else{
+			enc.edge = falling;
+         HIJACK_CompareConfig(hijackOutputModeSet);	//next is 0x00, for falling
+			dbg_led = 0;
+		}
 	}
-	else if( ( enc.data & ( 1 << bit_msk) ) && (CLR == enc.cur) ){
-	  	odd++;	//bit is one, odd cnt increacement
-		enc.factor = Div2;
-		enc.cur = SET;
-	}
-	else if( !( enc.data & ( 1 << bit_msk) ) && (SET == enc.cur) ){
-		enc.factor = Div2;
-		enc.cur = CLR;
-	}
-	else if( !( enc.data & ( 1 << bit_msk) ) && (CLR == enc.cur) ){
-		enc.factor = Div1;
-		if( 50 == index_sample )
-		  	enc.reverse = 1;
+	else{
+	  	if( rising == enc.edge ){
+		  	HIJACK_CompareConfig(hijackOutputModeSet);	//rising	
+	  	}
+	  	else{
+		  	HIJACK_CompareConfig(hijackOutputModeClear);	//falling
+	  	}
+		enc.state = state;	//state switch
 	}
 }
+
 
 /*
  * State machine for machester encoding.
  * Achieve wave phase.
 */
-
 void encode_machine(void)
 {
   	state_t sta = enc.state;
-	
+
 	switch(sta){
 		case Waiting:
-			if(us1_get_count() ){ 	//prepare for sta0
-				enc.state = Sta0; 	//next state is start bit
-				enc.factor = Div2;  //start bit is zero
-				enc.cur = CLR;
-				enc.data = us1_get_char();
-				index_sample = 0;
-			}
-			break;
-			//
-		case Sta0: 	//prepare for bit0
-		  	odd = 0;	//clear odd cnt
-		  	enc_parser(BIT0);
-	  		enc.state = Bit0;
-			break;
-			//
-		case Sta1:	//prepare for sta2
-		  	enc.state = Sta2;
-			break;
-			//
-	  	case Sta2:	//prepare for sta3
-		  	enc.state = Sta3;
-			break;
-			//
-		case Sta3:	//prepare for bit7, sta3 is SET
-		  	if( enc.data & (1 << BIT7) ){
-		  		enc.factor = Div1;
-				enc.cur = SET;
-				if( 50 == index_sample )
-		  			enc.reverse = 1;
+		  	if( 0 == ticker % 2){
+			  	dbg_led = 0;
+				if( uartGetAmount() ){
+					enc.data = uartGetChar();
+					enc.byte_rev = 1;
+					HIJACK_CompareConfig(hijackOutputModeSet);	//next is 0x00, falling
+					enc_odd = 0;
+				}
+				else{
+					HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, rising	
+					enc.byte_rev = 0;
+				}
 		  	}
 			else{
-				enc.factor = Div2;
-				enc.cur = CLR;	
+			  	if(enc.byte_rev){//new byte
+			  	 	HIJACK_CompareConfig(hijackOutputModeClear);	//falling
+					enc.state = Sta0;	//state switch
+				}
+				else{	//no byte
+					HIJACK_CompareConfig(hijackOutputModeSet);	//rising, keep in waiting state
+				}
 			}
-		  	enc.state = Bit7;
 			break;
-			//	
+			//
+		case Sta0: 	//prepare for sta1, rising edge
+			findParam(BIT0, Bit0);
+			break;
+			//
+		case Sta1:	//prepare for sta2, falling edge
+		  	if( 0 == ticker % 2){
+			 	HIJACK_CompareConfig(hijackOutputModeSet);		//next is 0x00, for falling
+			}
+			else{
+				HIJACK_CompareConfig(hijackOutputModeClear);	//falling
+				enc.state = Sta2; 	//state switch
+			}
+			break;
+			//
+	  	case Sta2:	//prepare for sta3, rising edge
+		  	if( 0 == ticker % 2){
+			 	HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, for rising
+			}
+			else{
+				HIJACK_CompareConfig(hijackOutputModeSet);	//rising
+				enc.state = Sta3;	//state switch
+			}
+			break;
+			//
+		case Sta3:	//prepare for bit7
+		  	findParam(BIT7, Bit7);
+			break;
+			//
 		case Bit0: 	//prepare for Bit1
-		  	enc_parser(BIT1);
-			enc.state = Bit1;
+		  	findParam(BIT1, Bit1);
 	    	break;
 			//
 		case Bit1: 	//prepare for Bit2
-		  	enc_parser(BIT2);
-			enc.state = Bit2;
+		  	findParam(BIT2, Bit2);
 	    	break;
 			//
 		case Bit2: 	//prepare for Bit3
-		  	enc_parser(BIT3);
-			enc.state = Bit3;
+		  	findParam(BIT3, Bit3);
 	    	break;
 			//
 		case Bit3: 	//prepare for Bit4
-		  	enc_parser(BIT4);
-			enc.state = Bit4;
+		  	findParam(BIT4, Bit4);
 	    	break;
 			//
 		case Bit4: 	//prepare for Bit5
-		  	enc_parser(BIT5);
-			enc.state = Bit5;
+		  	findParam(BIT5, Bit5);
 	    	break;
 			//
 		case Bit5: 	//prepare for Bit6
-		  	enc_parser(BIT6);
-			enc.state = Bit6;
+		  	findParam(BIT6, Bit6);
 	    	break;
 			//
 		case Bit6: 	//prepare for Bit7
-		  	enc_parser(BIT7);
-			enc.state = Bit7;
+		  	findParam(BIT7, Bit7);
 	    	break;
 			//
-		case Bit7: 	//prepare for Parity
-		  	if( ( 0 == ( odd % 2 ) ) && (SET == enc.cur) ){//there is even 1(s), output 1, cur is 1 	
-				enc.factor = Div1;
-				enc.cur = SET;
-				if( 50 == index_sample )
-					enc.reverse = 1;
-			}
-			else if( ( 0 == ( odd % 2 ) ) && (CLR == enc.cur) ){//there is even 1(s), output 1, cur is 0 	
-				enc.factor = Div2;
-				enc.cur = SET;
-			}
-			else if( ( 1 == ( odd % 2 ) ) && (CLR == enc.cur) ){//there is odd 1(s), output 0, cur is 0 	
-				enc.factor = Div1;
-				enc.cur = CLR;
-				if( 50 == index_sample )
-					enc.reverse = 1;
-			}
-			else if( ( 1 == ( odd % 2 ) ) && (SET == enc.cur) ){//there is odd 1(s), output 0, cur is 1
-				enc.factor = Div2;
-				enc.cur = CLR;
-			}
-			enc.state = Parity;
-	    	break;
-			//
-		case Parity:	//prepare for stop bit
-		  	if(SET == enc.cur){
-		  		enc.factor = Div1;
-				enc.cur = SET;
-				if( 50 == index_sample )
-					enc.reverse = 1;	
+		case Bit7: 	//prepare for parity
+		  	if( 0 == ticker % 2){
+				if( 0 == ( enc_odd%2 ) ){//there is even 1(s), output 1
+					enc.edge = rising;
+					HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, for rising
+					dbg_led = 1;
+				}
+				else{//there is odd 1(s), output 0
+					enc.edge = falling;
+					HIJACK_CompareConfig(hijackOutputModeSet);	//next is 0x00, for falling
+					dbg_led = 0;
+				}
 			}
 			else{
-				enc.factor = Div2;
-				enc.cur = CLR;
+				if( rising == enc.edge ){
+					HIJACK_CompareConfig(hijackOutputModeSet);	//rising	
+				}
+				else{
+					HIJACK_CompareConfig(hijackOutputModeClear);	//falling
+				}
+				enc.state = Parity;	//state switch
 			}
-			enc.state = Sto0;
-		  	break;
-            //
-		case Sto0:     //prepare for Waiting
-			enc.state = Waiting;
-			enc.factor = Div1;
-			enc.cur = SET;
+	    	break;
+			//
+		case Parity://prepare for stop bit, it is always 1
+		  	if( 0 == ticker % 2){
+				HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, for rising
+				dbg_led = 0 ;
+			}
+			else{
+				HIJACK_CompareConfig(hijackOutputModeSet);	//rising	
+				enc.state = Sto0;	//state switch
+			}
+        	break;
+			//
+		case Sto0:     //back to waiting
+			if( 0 == ticker % 2){
+				HIJACK_CompareConfig(hijackOutputModeClear);	//next is 0x80, for rising
+			}
+			else{
+				HIJACK_CompareConfig(hijackOutputModeSet);	//rising	
+				enc.state = Waiting;	//state switch
+			}
 			break;
 			//
+#if 0
+		case Sto1:		//go to waiting mode
+			enc.state = Waiting;
+			break;
+			//
+#endif
 		default:
 	  		break;
 	  		//
