1960caa5f26b40d5d888679534565cdd71ff16d0
kaiser.ren@gmail.com <kaiser.ren@gmail.com@4d133387-9cbb-5f73-990a-3ac0ff6c78c2>
2013-02-03 04:03:04 +0000

    my-project-hijack EFM32TG V0.02 1. NEW: tg210 can output encode wave.

--- trunk/ my-project-hihack --username kaiser.ren@gmail.com/energymicro/kits/EFM32TG_STK3300/examples/Senfone/tg210/main.h
+++ trunk/ my-project-hihack --username kaiser.ren@gmail.com/energymicro/kits/EFM32TG_STK3300/examples/Senfone/tg210/main.h
@@ -27,21 +27,11 @@
  *        Macro
  *----------------------------------------------------------------------------*/
 #define VER_MAJOR 0
-#define VER_MINOR 1
+#define VER_MINOR 2
 #define VER_PATCH ' '
 
 
 #define BOARD_MCK 7000000ul
-
-/* several carrier frequency definition. */
-#define HIJACK_CARRIER_FREQ_1KHZ	1000ul
-#define HIJACK_CARRIER_FREQ_2KHZ	2000ul
-#define HIJACK_CARRIER_FREQ_4KHZ	4000ul
-#define HIJACK_CARRIER_FREQ_8KHZ	8000ul
-#define HIJACK_CARRIER_FREQ_10KHZ 10000ul
-
-/* current carrier frequency definition. */
-#define HIJACK_CARRIER_FREQ_CONF HIJACK_CARRIER_FREQ_1KHZ
 
 /* PRESCALER: 128 */
 #define HIJACK_TMR_CLK_SRC_PRESCALER 1
